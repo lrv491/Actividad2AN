a7b4f59477507b357a3e4d5189c37a0b5e4f1dc1
lrv491 <lucia.ramirez491@comunidadunir.net>
2026-01-15 19:16:48 +0100

    Configuracion inicial del proyecto para CI/CD

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola y bienvenido");
+
+        for (int i=1;i<=5;i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
